d76355305c61a3827e8a740f96d5d26ecc02e647
zrz <occultskyrong@163.com>
2016-11-23 17:12:55 +0800

    更新 - 首页结构
    1、新增首页的内容
    2、改变h1、h2等标签的margin
    3、新增lodash、marked、cheerio等node-module

--- routes/web/index.js
+++ routes/web/index.js
@@ -5,16 +5,57 @@
 
 "use strict";
 
-var router = require('express').Router();
+var router = require('express').Router()
+    , cheerio = require('cheerio');
 
 router.get('/', function (req, res) {
-    res.render('./home/view', {
-        title: '首页'
+    var _md = markdown.marked('./readme.md')
+        , _table = '';
+    // 将其中的table拆出
+    var getTable = function () {
+        var _tArr = _md.split('<table>');
+        var isArr = function () {
+            return _tArr && _tArr instanceof Array && _tArr.length > 1;
+        };
+        if (isArr()) {
+            _tArr = _tArr[1].split('</table>');
+            if (isArr()) {
+                var $ = cheerio.load('<table class="table table-hover table-bordered table-condensed">'
+                    + _tArr[0] + '</table>');
+                (function ($trs) {
+                    for (var t in $trs) {
+                        if ($trs.hasOwnProperty(t) && !!t && parseInt(t) > 0) {
+                            var $tr = $trs.eq(t)
+                                , pro = parseFloat($tr.find('td').eq(2).text())
+                                , _uri = 'https://github.com/occultskyrong/blog/tree/master/'
+                                , $tda = $tr.find('td').eq(0).find('a');
+                            if (pro === 100) {
+                                $tr.addClass('success');
+                                $tda.attr('href', $tda.attr('href').replace(/\.\/public/g, ''));
+                            } else {
+                                $tda.attr('href', $tda.attr('href').replace(/\.\//g, _uri));
+                                if (pro === 0) {
+                                    $tr.addClass('warning');
+                                } else {
+                                    $tr.addClass('info');
+                                }
+                            }
+                        }
+                    }
+                }($('tr')));
+                _table = $.html();
+            }
+        }
+        return _table;
+    };
+    return res.render('./home/view', {
+        title: '首页 - blog'
+        , table: getTable()
     });
 });
 
 router.get('/history', function (req, res) {
-    res.render('./history/view', {
+    return res.render('./history/view', {
         title: '更新日志'
         , history: JSON.parse(require('fs').readFileSync('./public/static/history.json', 'utf8'))
     });
--- service/markdown/index.js
+++ service/markdown/index.js
@@ -6,19 +6,33 @@
 "use strict";
 
 var markdown = require('markdown').markdown
+    , marked = require('marked') // https://github.com/chjj/marked
     , fs = require('fs');
 
 module.exports = {
     // 获取静态资源地址
-    source:function (file) {
-        return file.split('./public')[1];
+    source: function (file) {
+        var fArr = file.split('./public');
+        return fArr && fArr instanceof Array && fArr.length > 1 && fArr[1] || fArr;
     }
-    // markdown转html
-    ,render: function (res, ejs, title, file) {
+    /**
+     * markdown转html，并render到页面
+     * @param res       response，返回体
+     * @param ejs       ejs地址
+     * @param title     标题
+     * @param file      file地址
+     * @param option    markdown配置
+     * @returns {*|String}
+     */
+    , render: function (res, ejs, title, file, option) {
+        if (option) {// 设置配置信息
+            marked.setOptions(option);
+        }
+        var _markdown = marked(fs.readFileSync(file, 'utf-8'));
         ejs = ejs ? ejs : './docs/_base/_template';
         return res.render(ejs, {
             title: title
-            , markdown: markdown.toHTML(fs.readFileSync(file, 'utf8'))
+            , markdown: _markdown
             , source: this.source(file)
         });
     }
@@ -26,8 +40,15 @@ module.exports = {
     , code: function (res, title, file) {
         return res.render('./docs/_base/_code', {
             title: title
-            , code: fs.readFileSync(file, 'utf8')
+            , code: fs.readFileSync(file, 'utf-8')
             , source: this.source(file)
         });
+    }
+    // markdown转html
+    , marked: function (file, option) {
+        if (option) {// 设置配置信息
+            marked.setOptions(option);
+        }
+        return marked(fs.readFileSync(file, 'utf-8'))
     }
 };
