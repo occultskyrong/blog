e609c4b2db09267b2acb3d46f2a0800192468a37
zrz <occultskyrong@163.com>
2016-12-30 18:29:40 +0800

    update leetcode

---
+++ public/docs/LeetCode_OJ/412_Fizz Buzz.js
@@ -0,0 +1,26 @@
+/**
+ * Created by zrz on 2016/12/29.
+ * Copyright© 2015-2020 DianDaInfo (https://github.com/diandainfo)
+ * @version 0.0.1 created
+ */
+
+"use strict";
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+    let _ = [];
+    for (let i = 1; i <= n; i++) {
+        let __ = '';
+        __ += i % 3 === 0 ? 'Fizz' : '';
+        __ += i % 5 === 0 ? 'Buzz' : '';
+        if (!__) {
+            __ += i;
+        }
+        _.push(__);
+    }
+    return _;
+};
+
+console.info(fizzBuzz(15));
---
+++ public/docs/LeetCode_OJ/448_Find All Numbers Disappeared in an Array.js
@@ -0,0 +1,19 @@
+/**
+ * Created by zrz on 2016/12/29.
+ * Copyright© 2015-2020 DianDaInfo (https://github.com/diandainfo)
+ * @version 0.0.1 created
+ */
+
+"use strict";
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+    let _ = [];
+    nums.sort();
+    return nums;
+};
+
+console.info(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]));
