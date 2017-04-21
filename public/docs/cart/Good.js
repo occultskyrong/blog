21847b942e5ec2a07a4c0c50bdaeee5c37a6d367
zrz <occultskyrong@163.com>
2017-04-21 19:11:46 +0800

    update  cart module

---
+++ public/docs/cart/Good.js
@@ -0,0 +1,62 @@
+/**
+ * Created by zrz on 2017/4/21.
+ * Copyright© 2015-2020 DianDaInfo (https://github.com/diandainfo)
+ * @version 0.0.1 created
+ */
+
+"use strict";
+
+const Mall = {
+    VIP_GRADE: 3        // 用户会员等级
+};
+
+// 商品 - 对象
+class Good {
+    constructor(good_id) {
+        this.good_id = good_id;
+    }
+
+    init(good) {
+        this.dadou = parseInt(good.dadou);                  // 赠豆
+        this.onSale = parseInt(good.onSale);                // 售卖状态
+        // 计算商品价格
+        this.setPrice(good);
+        this.price = this.getPrice();
+    }
+
+    // 写入价格相关数据
+    setPrice(good) {
+        this.belongsVip = parseInt(good.belongsVip);        // 会员商品
+        this.price = parseFloat(good.price);                // 价格
+        this.yjPrice = parseFloat(good.yjPrice);            // 原价
+        this.vipPrice = parseFloat(good.vipPrice);          // 会员价格
+    }
+
+    // 获取该商品价格
+    getPrice() {
+        let _price = 0;
+        if (this.onSale === 1) { // 正常销售
+            if (this.belongsVip === 0) { // 促销
+                _price = this.price;
+            } else if (this.belongsVip > 0) { // 会员
+                if (Mall.VIP_GRADE >= this.belongsVip) { // 会员等级大于商品需要的会员等级
+                    _price = this.vipPrice;
+                } else {
+                    _price = this.yjPrice;
+                }
+            }
+        } else {
+            _price = this.price;
+        }
+        return _price;
+    }
+
+    // 调整商品数量
+
+    // 刷新Dom
+    toDom() {
+
+    }
+}
+
+module.exports = Good;
--- public/docs/cart/index.js
+++ public/docs/cart/index.js
@@ -6,52 +6,6 @@
 
 "use strict";
 
-const Mall = {
-    VIP_GRADE: 3        // 用户会员等级
-};
-
-// 商品 - 对象
-class Good {
-    constructor(good_id) {
-        this.good_id = good_id;
-    }
-
-    init(good) {
-        this.dadou = parseInt(good.dadou);                  // 赠豆
-        this.onSale = parseInt(good.onSale);                // 售卖状态
-        // 计算商品价格
-        this.setPrice(good);
-        this.price = this.getPrice();
-    }
-
-    // 写入价格相关数据
-    setPrice(good) {
-        this.belongsVip = parseInt(good.belongsVip);        // 会员商品
-        this.price = parseFloat(good.price);                // 价格
-        this.yjPrice = parseFloat(good.yjPrice);            // 原价
-        this.vipPrice = parseFloat(good.vipPrice);          // 会员价格
-    }
-
-    // 获取该商品价格
-    getPrice() {
-        let _price = 0;
-        if (this.onSale === 1) { // 正常销售
-            if (this.belongsVip === 0) { // 促销
-                _price = this.price;
-            } else if (this.belongsVip > 0) { // 会员
-                if (Mall.VIP_GRADE >= this.belongsVip) { // 会员等级大于商品需要的会员等级
-                    _price = this.vipPrice;
-                } else {
-                    _price = this.yjPrice;
-                }
-            }
-        } else {
-            _price = this.price;
-        }
-        return _price;
-    }
-}
-
 // 活动 - 对象
 class Activity {
     constructor(activity_id) {
